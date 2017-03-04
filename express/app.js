19095aff8a260839cb52650a1777e730e7c88f11
Lyubomir Stoykov <lustoykov@gmail.com>
2017-03-04 20:30:24 +0100

    Express: Make react resources available

--- express/app.js
+++ express/app.js
@@ -29,6 +29,8 @@ app.use(require('node-sass-middleware')({
   sourceMap: true,
 }));
 app.use(express.static(path.join(__dirname, 'public')));
+// Make React Resources available
+app.use('/react', express.static(path.join(__dirname, '/../react/dist')));
 
 // setup Routes
 app.use('/', index);
