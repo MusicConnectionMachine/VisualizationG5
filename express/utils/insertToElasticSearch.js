d7b3f283ba2d1ef9a06ffe229fe54e75707d9a61
Chaoran Chen <mail@chaoran-chen.de>
2017-03-05 16:05:32 +0100

    Fix code style problems

--- express/utils/insertToElasticSearch.js
+++ express/utils/insertToElasticSearch.js
@@ -1,38 +1,38 @@
 #!/usr/bin/env node
-const
-  elasticsearch = require('elasticsearch'),
-  fs = require('fs'),
-  path = require('path');
+const elasticsearch = require('elasticsearch');
+const fs = require('fs');
+const path = require('path');
 
-const insertToElasticSearch = function () {
+
+const insertToElasticSearch = function insertToElasticSearch() {
   const client = new elasticsearch.Client({
-    host: 'elasticsearch:9200'
+    host: 'elasticsearch:9200',
   });
   const indices = [];
   const mockupDataDirectory = path.join(__dirname, '../../mock_data');
-  fs.readdirSync(mockupDataDirectory).forEach(function (elementName) {
+  fs.readdirSync(mockupDataDirectory).forEach((elementName) => {
     const filePath = path.join(mockupDataDirectory, elementName);
     const stat = fs.statSync(filePath);
     if (stat.isFile() && path.extname(filePath) === '.json') {
       indices.push({
         name: path.basename(elementName, '.json'),
-        elements: JSON.parse(fs.readFileSync(filePath, 'utf8'))
+        elements: JSON.parse(fs.readFileSync(filePath, 'utf8')),
       });
     }
   });
-  indices.forEach(function (index) {
-    client.indices.delete({index: index.name}).catch(function () {
+  indices.forEach((index) => {
+    client.indices.delete({ index: index.name }).catch(() => {
       // Index does not exist.
-    }).then(function () {
-      client.indices.create({index: index.name}).then(function () {
+    }).then(() => {
+      client.indices.create({ index: index.name }).then(() => {
         const body = [];
-        index.elements.forEach(el => {
+        index.elements.forEach((el) => {
           const id = el.id;
-        delete el.id;
-        body.push({index: {_index: index.name, _type: 'external', _id: id}}, el);
-      });
+          delete el.id;
+          body.push({ index: { _index: index.name, _type: 'external', _id: id } }, el);
+        });
         client.bulk({
-          body: body
+          body,
         });
       });
     });
@@ -40,4 +40,3 @@ const insertToElasticSearch = function () {
 };
 
 module.exports = insertToElasticSearch;
-
