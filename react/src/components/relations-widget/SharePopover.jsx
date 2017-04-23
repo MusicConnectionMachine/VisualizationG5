bc180281f895766ce8236e7dc725225cbfee4ce7
Martin Mihaylov <martin.mihaylov@tum.de>
2017-04-23 17:45:42 +0200

    Implement popover close by clicking outside

--- react/src/components/relations-widget/SharePopover.jsx
+++ react/src/components/relations-widget/SharePopover.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Popover, PopoverTitle, PopoverContent, Button } from 'reactstrap';
 import { ShareButtons, generateShareIcon } from 'react-share';
+import ClickOutside from 'react-click-outside';
 
 export default class SharePopover extends React.Component {
   constructor(props) {
@@ -34,43 +35,46 @@ export default class SharePopover extends React.Component {
         target={target}
         isOpen={isOpen}
       >
-        <PopoverTitle className="popover__title">
-          <Button
-            onClick={() => close()}
-            color="link"
-            className="popover__close-button close-icon"
-          />
-          Share
-          <Button className="popover__balance-button" />
-        </PopoverTitle>
-        <PopoverContent className="popover__content">
-          <div>
-            {'Just found on #MusicConnectionMachine:'}
-            <br />
-            {`${relation.entity1} ${relation.relation} ${relation.entity2}`}
-          </div>
-          <FacebookShareButton
-            className="popover__content__share-button"
-            url={url}
-            title="MusicConnectionMachine"
-            description={shareText}
-          >
-            <FacebookIcon size={64} />
-          </FacebookShareButton>
-          <TwitterShareButton
-            className="popover__content__share-button"
-            url={url}
-            title={shareText}
-          >
-            <TwitterIcon size={64} />
-          </TwitterShareButton>
-          <GooglePlusShareButton
-            className="popover__content__share-button"
-            url={url}
-          >
-            <GooglePlusIcon size={64} />
-          </GooglePlusShareButton>
-        </PopoverContent>
+        <ClickOutside onClickOutside={() => close()}>
+          <PopoverTitle className="popover__title">
+            <Button
+              onClick={() => close()}
+              color="link"
+              className="popover__close-button close-icon"
+            />
+            Share
+            <Button className="popover__balance-button" />
+          </PopoverTitle>
+
+          <PopoverContent className="popover__content">
+            <div>
+              {'Just found on #MusicConnectionMachine:'}
+              <br />
+              {`${relation.entity1} ${relation.relation} ${relation.entity2}`}
+            </div>
+            <FacebookShareButton
+              className="popover__content__share-button"
+              url={url}
+              title="MusicConnectionMachine"
+              description={shareText}
+            >
+              <FacebookIcon size={64} />
+            </FacebookShareButton>
+            <TwitterShareButton
+              className="popover__content__share-button"
+              url={url}
+              title={shareText}
+            >
+              <TwitterIcon size={64} />
+            </TwitterShareButton>
+            <GooglePlusShareButton
+              className="popover__content__share-button"
+              url={url}
+            >
+              <GooglePlusIcon size={64} />
+            </GooglePlusShareButton>
+          </PopoverContent>
+        </ClickOutside>
       </Popover>
     );
   }
