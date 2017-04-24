42b2f8d4c937566214e2395d1034e66720f7b0b0
Chaoran Chen <mail@chaoran-chen.de>
2017-04-24 02:05:22 +0200

    In the mockup-mode, further parameters will not be required anymore.

--- react/src/StartupService.jsx
+++ react/src/StartupService.jsx
@@ -9,12 +9,18 @@ export default class StartupService {
   static start(Application, additionalProps) {
     if (StartupService.isMockupMode()) {
       environment = 'dev-mockups';
+      StartupService.renderApplication(Application, 'artists', '-mockups-', additionalProps);
+    } else {
+      StartupService.getEntityInformation().then(({ entityType, entityId }) => {
+        StartupService.renderApplication(Application, entityType, entityId, additionalProps);
+      });
     }
+  }
 
-    StartupService.getEntityInformation().then(({ entityType, entityId }) => {
-      const root = document.getElementById('react-app');
-      ReactDOM.render(<Application entityType={entityType} entityId={entityId} {...additionalProps} />, root);
-    });
+
+  static renderApplication(Application, entityType, entityId, additionalProps) {
+    const root = document.getElementById('react-app');
+    ReactDOM.render(<Application entityType={entityType} entityId={entityId} {...additionalProps} />, root);
   }
 
 
@@ -42,14 +48,14 @@ export default class StartupService {
   static mapImslpLink(imslpLink) { // eslint-disable-line
     // TODO Call the API to get the real information.
     let windowUrl = window.location.href;
-    let indexToCutOut = windowUrl.indexOf("wiki") + 5;
-    //If Composer
-    if (windowUrl.indexOf("Category") >= 0) {
-      indexToCutOut = indexToCutOut + 9;
+    let indexToCutOut = windowUrl.indexOf('wiki') + 5;
+    // If Composer
+    if (windowUrl.indexOf('Category') >= 0) {
+      indexToCutOut += 9;
       windowUrl = windowUrl.slice(indexToCutOut);
-      let lastname = windowUrl.slice(0,url.indexOf(","));
-      let firstname = windowUrl.slice(url.indexOf(",")+2);
-      let title = firstname + '_' +lastname;
+      const lastname = windowUrl.slice(0, url.indexOf(','));
+      const firstname = windowUrl.slice(url.indexOf(',') + 2);
+      let title = `${firstname}_${lastname}`;
       title = title.replace(new RegExp('_', 'g'), ' ');
 
       const url = `http://mcmapi.azurewebsites.net/search/entities?q=${title}&limit=1`;
@@ -59,28 +65,23 @@ export default class StartupService {
         },
       })
         .then(res => res.json())
-        .then(res => {
-          return res[0].artists[0];
-        });
+        .then(res => res[0].artists[0]);
     }
-    //If Composition
-    else {
-      windowUrl = windowUrl.slice(indexToCutOut);
-      let composition = windowUrl.slice(0,windowUrl.indexOf('(')-1);
-      composition = composition.replace(new RegExp('_', 'g'), ' ');
-      const title = composition;
+    // If Composition
 
-      const url = `http://mcmapi.azurewebsites.net/search/entities?q=${title}&limit=1`;
-      return fetch(url, {
-        headers: {
-          'API-KEY': 123,
-        },
-      })
+    windowUrl = windowUrl.slice(indexToCutOut);
+    let composition = windowUrl.slice(0, windowUrl.indexOf('(') - 1);
+    composition = composition.replace(new RegExp('_', 'g'), ' ');
+    const title = composition;
+
+    const url = `http://mcmapi.azurewebsites.net/search/entities?q=${title}&limit=1`;
+    return fetch(url, {
+      headers: {
+        'API-KEY': 123,
+      },
+    })
         .then(res => res.json())
-        .then(res => {
-          return res[0].works[0];
-        });
-    }
+        .then(res => res[0].works[0]);
   }
 
 
